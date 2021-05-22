407d5c8179bd38fc8b049d4d05cbebb8954715f7
emilyl <emily.lukas@gmail.com>
2021-05-22 10:41:38 -0700

    initial screens

---
+++ app/src/main/java/com/example/baik/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.baik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
