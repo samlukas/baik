90c8c37b2115967701f9f779652d2eea57e2446d
emilyl <emily.lukas@gmail.com>
2021-05-22 11:13:45 -0700

    fix signin button

--- app/src/main/java/com/example/baik/MainActivity.java
+++ app/src/main/java/com/example/baik/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        signInButton = findViewById(R.id.sign_in_button);
+        signInButton = (Button) findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
